6acd03c1b45b55a197ab89332a893f7d20c824e8
Joylson <joylsont@gmail.com>
2018-03-31 09:44:28 -0300

    Modelo de usuario

--- Crud ADO.NET/DTO/Usuario.cs
+++ Crud ADO.NET/DTO/Usuario.cs
@@ -8,5 +8,9 @@ namespace DTO
 {
     public class Usuario
     {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Senha { get; set; }
+        public string Email { get; set; }
     }
 }
